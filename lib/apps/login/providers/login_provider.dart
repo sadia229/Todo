0ae6d7251bcd033c71a61cdae05bbb6a50b49223
Sadia Afrin <108749310+sadia229@users.noreply.github.com>
2023-02-19 04:10:43 -0800

    add toast

--- lib/apps/login/providers/login_provider.dart
+++ lib/apps/login/providers/login_provider.dart
@@ -24,6 +24,7 @@ class AuthNotifier {
     } on FirebaseAuthException catch (e) {
       if (e.code == 'weak-password') {
         print('The password provided is too weak.');
+
       } else if (e.code == 'email-already-in-use') {
         print('The account already exists for that email.');
       }
